d109f2d34bf0a66e93dc1264a87aa884d9aaf9ba
mattAbe <mattab7@gmail.com>
2020-03-06 23:58:12 -0600

    CRUD for user

--- API/README.md
+++ API/README.md
@@ -2,3 +2,8 @@
 
 ## Steps
 * Run "npm start" to run the API on port 8080.
+
+## Requirements
+* Have mongo installed and running locally.
+
+api-routes.js will specify all the available URLs.
--- API/api-routes.js
+++ API/api-routes.js
@@ -1,4 +1,5 @@
 let router = require('express').Router();
+var userController = require('./UserController');
 
 router.get('/', function (req, res) {
     res.json({
@@ -6,5 +7,18 @@ router.get('/', function (req, res) {
         message: 'Welcome to RazettoStone\'s API!'
     });
 });
+
+// Add new user to DB -- Pass values in request's body
+router.route('/users/create')
+    .post(userController.create);
+// Fetch all users from DB
+router.route('/users')
+    .get(userController.index);
+// Fetch specific user from DB
+router.route('/users/:user_id')
+    .get(userController.view);
+// Update specific user from DB -- Pass values to be updated in request's body
+router.route('/users/update/:user_id')
+    .put(userController.update)
 
 module.exports = router;
