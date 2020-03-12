50dfb6ffc711b7dd2ec5d530f96f81c3ca0cf83a
liamlan <liamlan227@gmail.com>
2020-03-12 14:13:03 -0500

    Revert "commit"
    This reverts commit ec3baa45fee893832aadd45b86ce12275b008326.

--- API/PostSchema.js
+++ API/PostSchema.js
@@ -1,58 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
-// TODO add schema for comments
-const CommentSchema = new Schema({
-
-});
-
-// TODO add schema for flags
-const FlagSchema = new Schema({
-
-});
-
-// TODO add schema for translations
-const TranslationSchema = new Schema({
-
-});
-
 const PostSchema = new Schema({
-    title: {
-        type: String,
-        required: [true, 'Title field is required']
-    },
-    language: {
-        type: String,
-        required: [true, 'Language field is required']
-    },
-    original_text: {
-        type: String,
-        required: [true, 'original_text field is required']
-    },
-    user_id:{
-        type: String,
-        required: [true, 'user_id field is required']
-    },
-    created_date:{
-        type: Date,
-        required: [true, 'created_date field is required']
-    },
-    upvotes:{
-        type: [String],
-        required: [true, 'upvotes field is required']
-    },
-    downvotes:{
-        type: [String],
-        required: [true, 'downvotes field is required']
-    },
-    tags:{
-        type: [String],
-        required: [true, 'tags field is required']
-    },
-    comments: [CommentSchema],
-    flags: [FlagSchema],
-    translations: [TranslationSchema]
+
 });
 
 const Post = mongoose.model('post', PostSchema);
