4b69600fc105b5a032ee06b45f8e7dacff795ca7
mattAbe <mattab7@gmail.com>
2020-03-30 22:31:44 -0500

    adding status codes and unit tests

--- API/__tests__/users.test.js
+++ API/__tests__/users.test.js
@@ -6,16 +6,19 @@ const supertest = require('supertest');
 const app = require('../');
 const mongoose = require('mongoose')
 
+var userId;
 
 describe("Testing the users endpoint", () => {
 
 	it("can add user successfully", async () => {
 		const response = await supertest(app).post('/users')
 			.send({userName: "TestUser1",
-			email: "TestUser1@email.com",
-			dateCreated: "2020-03-25T03:52:32.187Z",
-			languages: ["english"],
-			oAuthId: "TestUser1_OAuthId"})
+				email: "TestUser1@email.com",
+				dateCreated: "2020-03-25T03:52:32.187Z",
+				languages: ["english"],
+				oAuthId: "TestUser1_OAuthId"})
+
+		userId = response.body.data._id;
 
 		expect(response.status).toBe(200);
 		expect(response.body.message).toEqual("New user created!");
@@ -25,7 +28,83 @@ describe("Testing the users endpoint", () => {
 		expect(response.body.data.languages).toEqual(["english"]);
 		expect(response.body.data.oAuthId).toEqual("TestUser1_OAuthId");
 	});
+
+	it("cannot add user without username", async () => {
+		const response = await supertest(app).post('/users')
+			.send({email: "TestUser1@email.com",
+				dateCreated: "2020-03-25T03:52:32.187Z",
+				languages: ["english"],
+				oAuthId: "TestUser1_OAuthId"})
+
+		expect(response.status).toBe(422);
+		expect(response.body.message).toEqual("Need username. ");
+		expect(response.body.status).toEqual("failed");
+	});
 	
+	it("cannot add user without email", async () => {
+		const response = await supertest(app).post('/users')
+			.send({userName: "TestUser1",
+				dateCreated: "2020-03-25T03:52:32.187Z",
+				languages: ["english"],
+				oAuthId: "TestUser1_OAuthId"})
+
+		expect(response.status).toBe(422);
+		expect(response.body.message).toEqual("Need email. ");
+		expect(response.body.status).toEqual("failed");
+	});
+
+	it("cannot add user without username and email", async() => {
+		const response = await supertest(app).post('/users')
+			.send({dateCreated: "2020-03-25T03:52:32.187Z",
+				languages: ["english"],
+				oAuthId: "TestUser1_OAuthId"})
+
+		expect(response.status).toBe(422);
+		expect(response.body.message).toEqual("Need username. Need email. ")
+		expect(response.body.status).toEqual("failed");
+	})
+
+	it("cannot add user with same existing username", async () => {
+		const response = await supertest(app).post('/users')
+			.send({userName: "TestUser1",
+			email: "TestUser1notsame@email.com",
+			dateCreated: "2020-03-25T03:52:32.187Z",
+			languages: ["english"],
+			oAuthId: "TestUser1_OAuthId"})
+
+		expect(response.status).toBe(409);
+		expect(response.body.message).toEqual("Username already exists. ");
+		expect(response.body.status).toEqual("failed");
+	});
+	
+	it("cannot add user with same existing email", async () => {
+		const response = await supertest(app).post('/users')
+			.send({userName: "TestUser1notsame",
+			email: "TestUser1@email.com",
+			dateCreated: "2020-03-25T03:52:32.187Z",
+			languages: ["english"],
+			oAuthId: "TestUser1_OAuthId"})
+
+		expect(response.status).toBe(409);
+		expect(response.body.message).toEqual("Email already exists. ");
+		expect(response.body.status).toEqual("failed");
+	});
+
+	it("cannot add user with same existing username and email", async() => {
+		const response = await supertest(app).post('/users')
+			.send({userName: "TestUser1",
+			email: "TestUser1@email.com",
+			dateCreated: "2020-03-25T03:52:32.187Z",
+			languages: ["english"],
+			oAuthId: "TestUser1_OAuthId"})
+
+		expect(response.status).toBe(409);
+		// Using toContain rather than toEqual because the API validates this asynchronously hence message won't be in same order always
+		expect(response.body.message).toContain("Email already exists. ")
+		expect(response.body.message).toContain("Username already exists. ")
+		expect(response.body.status).toEqual("failed");
+	})
+
 	it("fetch all users successfully", async () => {
 
 		const response = await supertest(app).get('/users');
@@ -38,6 +117,35 @@ describe("Testing the users endpoint", () => {
 		expect(response.body.data[0].dateCreated).toEqual("2020-03-25T03:52:32.187Z");
 		expect(response.body.data[0].languages).toEqual(["english"]);
 		expect(response.body.data[0].oAuthId).toEqual("TestUser1_OAuthId");
+	});
+
+	it("fetch single user successfully", async () => {
+
+		const response = await supertest(app).get('/users/'+userId);
+
+		expect(response.status).toBe(200);
+        expect(response.body.status).toBe("success");
+		expect(response.body.message).toBe("User retrieved successfully");
+		expect(response.body.data.userName).toEqual("TestUser1");
+		expect(response.body.data.email).toEqual("TestUser1@email.com");
+		expect(response.body.data.dateCreated).toEqual("2020-03-25T03:52:32.187Z");
+		expect(response.body.data.languages).toEqual(["english"]);
+		expect(response.body.data.oAuthId).toEqual("TestUser1_OAuthId");
+	});
+
+	it("update single user successfully", async () => {
+
+		const response = await supertest(app).put('/users/'+userId)
+			.send({email: "newEmail@gmail.com"});
+
+		expect(response.status).toBe(200);
+        expect(response.body.status).toBe("success");
+		expect(response.body.message).toBe("User Info updated");
+		expect(response.body.data.userName).toEqual("TestUser1");
+		expect(response.body.data.email).toEqual("newEmail@gmail.com");
+		expect(response.body.data.dateCreated).toEqual("2020-03-25T03:52:32.187Z");
+		expect(response.body.data.languages).toEqual(["english"]);
+		expect(response.body.data.oAuthId).toEqual("TestUser1_OAuthId");
 	});
 });
 
--- API/users/UserController.js
+++ API/users/UserController.js
@@ -1,14 +1,34 @@
 let User = require('./UserModel');
 let auth = require('../auth/auth');
 
+// Function: Checking DB for duplicate entries
+// Input: userName and email
+// Output: errorMessage. If no error, then errorMessage is empty
+let validateUser = async function(req_UserName, req_Email) {
+    errorMessage = '';
+    await User.find({userName: req_UserName}, function (err, docs) {
+        if (docs != undefined && docs.length){
+            console.log('userName already exists in schema');
+            errorMessage = errorMessage.concat('Username already exists. ');
+        }}).then(
+        await User.find({email: req_Email}, function (err, docs) {
+        if (docs != undefined &&  docs.length){
+            console.log('email already exists in schema');
+            errorMessage = errorMessage.concat('Email already exists. ');
+        }}))
+    console.log(errorMessage)
+    return errorMessage;
+}
 
 exports.index = function (req, res) {
+    console.log('Attempting to fetch all users.')
     User.get(function (err, users) {
         if (err) {
-            res.json({
-                status: "error",
+            res.status(500).json({
+                status: "failed",
                 message: err,
             });
+            return res;
         }
         res.json({
             status: "success",
@@ -18,8 +38,8 @@ exports.index = function (req, res) {
     });
 };
 
-exports.create = function (req, res) {
-    console.log('Attempting to insert an user')
+exports.create = async function (req, res) {
+    console.log('Attempting to insert an user.')
     var user = new User({
         userName: req.body.userName,
         email: req.body.email,
@@ -28,31 +48,70 @@ exports.create = function (req, res) {
         oAuthId: req.body.oAuthId
     });
     
-	//not actualy verifying
-	//if(auth.verify(user.oAuthId)) {
-	
-		console.log("get called");
-	
-		user.save(function (err) {
-			if (err) {
-				res.json(err);
-			} else {
-				res.json({
-					message: 'New user created!',
-					data: user
-				});
-			}
-		});
-	//}
+    var errorMessage = '';
+    // Checking for required parameters
+    if (req.body.userName == undefined || !req.body.userName.trim())
+    {
+        console.log('Request did not have userName');
+        errorMessage = errorMessage.concat('Need username. ');
+    }
+    if (req.body.email == undefined || !req.body.email.trim())
+    {
+        console.log('Request did not have email');
+        errorMessage = errorMessage.concat('Need email. ');
+    }
+    if (errorMessage.length)
+    {
+        res.status(422).json({
+            message: errorMessage,
+            status: 'failed'
+        })
+        return res;
+    }
+    // Resetting error message for future use
+    errorMessage = '';
+
+    errorMessage = await validateUser(req.body.userName, req.body.email)
+    
+    if (errorMessage != undefined && errorMessage.length)
+    {
+        res.status(409).json({
+            message: errorMessage,
+            status: 'failed'
+        })
+        return res;
+    }
+    else
+    {
+        //not actualy verifying
+        //if(auth.verify(user.oAuthId)) {
+        
+            user.save(function (err) {
+                console.log('save called')
+                if (err) {
+                    res.status(500).json(err);
+                } else {
+                    res.json({
+                        message: 'New user created!',
+                        data: user
+                    });
+                }
+            });
+        //}
+    }
 };
 
 exports.view = function (req, res) {
     console.log('Attempting to retrieve user from DB')
     User.findById(req.params.user_id, function (err, user) {
         if (err)
-            res.send(err);
+        {
+            res.status(500).send(err);
+            return res;
+        }
         res.json({
-            message: 'User details loading..',
+            status: 'success',
+            message: 'User retrieved successfully',
             data: user
         });
     });
@@ -62,7 +121,10 @@ exports.update = function (req, res) {
 User.findById(req.params.user_id, function (err, user) {
         console.log("Attempting to update user")
         if (err)
-            res.send(err);
+        {
+            res.status(500).send(err);
+            return res;
+        }
         if (req.body.userName) user.userName = req.body.userName
         if (req.body.email) user.email = req.body.email
         // Send the whole language array for adding or removing languages
@@ -71,8 +133,12 @@ User.findById(req.params.user_id, function (err, user) {
 
         user.save(function (err) {
             if (err)
-                res.json(err);
+            {
+                res.status(500).send(err);
+                return res;
+            }
             res.json({
+                status: 'success',
                 message: 'User Info updated',
                 data: user
             });
