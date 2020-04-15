c5f2d784c92252f89176a7b7ee7d65d62a1446d8
liamlan <liamlan227@gmail.com>
2020-04-15 00:29:56 -0500

    Add CommentController

--- API/posts/PostRoutes.js
+++ API/posts/PostRoutes.js
@@ -1,9 +1,9 @@
 let router = require('express').Router();
 let postController = require('./PostController');
 let voteController = require('./VoteController');
+let commentController = require('./CommentController');
 
-// I think postController can be broken down but I'm a lazy 
-
+// These will be unser postController
 // create post
 router.route('/').post(postController.create);
 // get one post
@@ -12,15 +12,24 @@ router.route('/:post_id').get(postController.view);
 router.route('/:post_id/translations/').post(postController.addTranslation);
 // search for a post
 router.route('/search/:search_string').get(postController.search);
+//Flag
+router.route('/:post_id/translations/:translation_id/flag').put(postController.flagTranslation);
+// get one translation
+router.route('/:post_id/translations/:translation_id').get(postController.getOneTranslation);
+// get list
+router.route("").get(postController.listPosts);
+router.route("/:post_id/translations").get(postController.listTranslations);
 
+
+// Comments and Replies will be under commentController
 //Comments
-router.route('/:post_id/comments').post(postController.commentOnPost);
-router.route('/:post_id/translations/:translation_id/comments/').post(postController.commentOnTranslation);
-
+router.route('/:post_id/comments').post(commentController.commentOnPost);
+router.route('/:post_id/translations/:translation_id/comments/').post(commentController.commentOnTranslation);
 //Replies
-router.route('/:post_id/comments/:comment_id/replies').post(postController.replyToPostComment);
-router.route('/:post_id/translations/:translation_id/comments/:comment_id/replies').post(postController.replyToTranslationComment);
+router.route('/:post_id/comments/:comment_id/replies').post(commentController.replyToPostComment);
+router.route('/:post_id/translations/:translation_id/comments/:comment_id/replies').post(commentController.replyToTranslationComment);
 
+// Votes will be under voteController
 //Votes
 router.route('/:post_id/vote').put(voteController.votePost);                                     // vote on post
 router.route('/:post_id/translations/:translation_id/vote').put(voteController.voteTranslation); // vote on translation
@@ -28,19 +37,6 @@ router.route('/:post_id/comments/:comment_id/vote').put(voteController.votePostC
 router.route('/:post_id/translations/:translation_id/comments/:comment_id/vote').put(voteController.voteTranslationComment); // vote on translation comment
 router.route('/:post_id/comments/:comment_id/replies/:reply_id/vote').put(voteController.votePostCommentReply);
 router.route('/:post_id/translations/:translation_id/comments/:comment_id/replies/:reply_id/vote').put(voteController.voteTranslationCommentReply);
-
-//Flag
-router.route('/:post_id/translations/:translation_id/flag').put(postController.flagTranslation);
-
-// get one translation
-router.route('/:post_id/translations/:translation_id').get(postController.getOneTranslation);
-
-
-// get list
-router.route("").get(postController.listPosts);
-router.route("/:post_id/translations").get(postController.listTranslations);
-
-
 
 
 module.exports = router;
